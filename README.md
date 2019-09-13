80c50965ebb58c13340ce9c2564bdd1ddd470fcc
Marcus <McKinley>
2019-09-12 23:50:27 -0400

    pushing all code to master

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # AcronymLookup
 Python script to quickly find out what common IT acronyms stand for
+
+Getting Started
+-----------------------------------------------------------------------------------
+- Download python if you have not already. I am currently using version 2.7.15
+- Clone this repository: git clone https://github.com/marcusmckinley/AcronymLookup.git
+
+File Structure: 
+-----------------------------------------------------------------------------------
+array.py
+
+    - Acronym class 
+      + holds two attributes: an acronym and what it stands for
+    
+    - all_acronyms array 
+      + holds Acronym class objects
+
+acronyms.py
+
+    - clear function 
+      + prints a new line character 100 times 
+        
+    - linear_search
+      + takes in an acronym and an array as arguments
+      + searches the array for the given acronym and returns what the acronym stands for or an error message
+      
+    - program loop
+      + a loop that never terminates, that asks the user for an acronym and returns what it stands for
+
+Running the Code: 
+-----------------------------------------------------------------------------------
+1) Navigate to the AcronymLookup directory in the terminal 
+2) Type python acronyms.py and press enter
---
+++ acronyms.py
@@ -0,0 +1,50 @@
+import array
+import os
+
+#Postcondition: clears output screen
+def clear():
+    print "\n" * 100
+
+#Postcondition: returns a string result of what given acronym stands for or and Error Message
+def linear_search(acronym, arr):
+    if (len(arr) == 0):
+        return "Empty List"
+    else:
+        for val in arr:
+            if (val.acronym == acronym):
+                return val.stands_for
+
+        return "Acronym not found"
+
+#Postcondition: returns a string result of what given acronym stands for or and Error Message    
+def binary_search(acronym, arr, start, end):
+    mid = (start + end) / 2
+    if (len(arr) == 0):
+        return "Empty List"
+
+    if (start == end):
+        return ("Acronym not found" if arr[start].acronym != acronym else arr[start].stands_for)
+
+    if (acronym == arr[start].acronym):
+        return arr[start].stands_for
+    elif (acronym == arr[end].acronym):
+        return arr[end].stands_for
+    elif (acronym == arr[mid].acronym):
+        return arr[mid].stands_for
+    else:
+        if (acronym > arr[mid].acronym):
+            return binary_search(acronym, arr, mid + 1, end)
+        else:
+            return binary_search(acronym, arr, start, mid - 1)
+    
+
+done = False
+while (False == done):
+    print("Enter an acronym: ")
+    acronym = raw_input()
+    clear()
+    print("Press Enter to continue\n\n\n")
+    #print(acronym + " stands for " + linear_search(acronym, array.all_acronyms))
+    print(acronym + " stands for " + binary_search(acronym, array.all_acronyms, 0, len(array.all_acronyms) - 1))
+    acronym = raw_input()
+    clear()
